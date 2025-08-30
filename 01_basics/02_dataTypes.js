cbcd7b36e7aa17c5a5bea13bcd1d4e7b703f5525
Suryansh Tiwari <168472042+SuryanshTiwari18@users.noreply.github.com>
2025-08-30 15:12:58 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all JS code as newer version
+
+// alert("hello") //we are using nodejs , not browser
+
+console.log(3 + 3); //code readability should be high
+console.log("suryansh")
+
+let name = "suryansh"
+let age = 18
+let isLoggedIn = false
+let state = null
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true / false
+//null => standalone value  //type of null is object
+//undefined //type of undefined is undefined
+//symbol => unique
+
+//object 
+
+console.log(typeof "suryansh")
