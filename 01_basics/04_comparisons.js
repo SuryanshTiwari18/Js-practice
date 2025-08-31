7a7ca6922c81d029ec98c828e78917468180b904
Suryansh Tiwari <168472042+SuryanshTiwari18@users.noreply.github.com>
2025-08-31 06:26:25 +0000

    comparisons

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1);
+// console.log(22 >= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+// equality check == and comparisons > < >= <= work differently
+//comparisons convert null to a number, treating it as a 0.
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+// console.log(undefined != 0);
+
+// === 
+console.log("2" === 2);
