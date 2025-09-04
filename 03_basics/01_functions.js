dcee21f1f18c83690e55f141b607747e12f1d1e2
Suryansh Tiwari <168472042+SuryanshTiwari18@users.noreply.github.com>
2025-09-04 01:46:32 +0000

    functions part 1

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,28 @@
+function sayMyName(){
+    console.log("h");
+    console.log("e");
+    console.log("i");
+    console.log("s");
+    console.log("e");
+    console.log("n");
+    console.log("b");
+    console.log("e");
+    console.log("r");
+    console.log("g");
+}
+
+// sayMyName // reference
+// sayMyName() //execution
+
+// function addTwoNumbers(num1, num2){
+//     console.log(num1 + num2);
+// }
+
+function addTwoNumbers(num1, num2){
+    // let result = num1 + num2
+    // return (result);
+    return num1 + num2;
+}
+
+const result = addTwoNumbers(5, 6)
+console.log(result);
