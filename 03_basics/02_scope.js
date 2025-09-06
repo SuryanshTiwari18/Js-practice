b738df34969566687824bff167c0276c54315707
Suryansh Tiwari <168472042+SuryanshTiwari18@users.noreply.github.com>
2025-09-06 12:56:46 +0000

    scope part two

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -3,10 +3,44 @@ let a  = 300
 if(true){
     let a = 10
     const b = 20
-    console.log("INNER: ", a);
+    // console.log("INNER: ", a);
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
 
+function one(){
+    const userName = "hitesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(userName);
+    }
+    // console.log(website);
+
+    two()
+}
+// one()
+
+if(true){
+    const userName = "hitesh"
+    if(userName==="hitesh"){
+        const website = " youtube"
+        // console.log(userName + website);
+    }
+    // console.log(website);
+}
+// console.log(userName);
+
+// +++++++++++++ intreresting ++++++++++++++
+
+console.log(addOne(5));
+function addOne(num){
+    return num+1
+}
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
