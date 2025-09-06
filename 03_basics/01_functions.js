b51e4641ded17fb7937d556f796508673c6fb4c1
Suryansh Tiwari <168472042+SuryanshTiwari18@users.noreply.github.com>
2025-09-06 11:06:47 +0000

    functions part two

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -35,5 +35,34 @@ function loginUserMessage(useranme = "Sam"){
     return `${useranme} just logged in`;
 }
 // console.log(loginUserMessage("Hitesh"))
-console.log(loginUserMessage());
+// console.log(loginUserMessage());
 
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500, 2000))
+
+const user = {
+    username: "hitesh",
+    prices: 199
+}
+
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`);
+    
+}
+// handleObject(user)
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
