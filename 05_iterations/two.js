f2c7857484c7d8186a31b7dfcc7d59353ab23a5c
Suryansh Tiwari <168472042+SuryanshTiwari18@users.noreply.github.com>
2025-09-21 10:34:28 +0000

    while and do-while

---
+++ 05_iterations/two.js
@@ -0,0 +1,19 @@
+let i = 0;
+// while (i <= 10) {
+//     console.log(`value of i is: ${i}`);
+//     i++;
+// }
+
+// let myArray = ["flash", "batman", "superman"]
+// let arr = 0;
+// while(arr < myArray.length){
+//     console.log(myArray[arr]);
+//     arr++;
+// }
+
+let score = 11;
+
+do {
+    console.log(`your score is: ${score}`);
+    score++;
+} while (score <= 10);
