966acaa797b0ace4a54f854df9838e2c0eee439e
Suryansh Tiwari <suryanshtiwari1811@gmail.com>
2026-02-22 16:59:13 +0530

    events and async

---
+++ 07_projects/Project_6/bgcolor.js
@@ -0,0 +1,23 @@
+//generate random color
+
+const randomColor = function(){
+    const hex = "0123456789ABCDEF";
+    let color = "#"
+    for(let i=0; i<6; i++){
+        color += hex[Math.floor(Math.random() * 16)]
+    }
+    return color;
+}
+let intervalId;
+const changeColor = function(){
+    intervalId = setInterval(changeBgColor, 1000);
+    function changeBgColor(){
+        document.body.style.backgroundColor = randomColor();
+    }
+}
+
+const stopChangeColor = function(){
+    clearInterval(intervalId);
+}
+document.querySelector("#start").addEventListener('click', changeColor)
+document.querySelector("#stop").addEventListener('click', stopChangeColor)
